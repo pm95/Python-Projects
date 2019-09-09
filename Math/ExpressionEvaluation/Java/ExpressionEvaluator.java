e0e77703f97f1f585aeee4d81bacf60fbf7c118d
Pietro Malky <pietromalky@gmail.com>
2019-09-09 17:28:51 -0500

    finished high and low precedence evaluation functinos

--- Math/ExpressionEvaluation/Java/ExpressionEvaluator.java
+++ Math/ExpressionEvaluation/Java/ExpressionEvaluator.java
@@ -3,28 +3,89 @@
 // Date: 09/09/2019
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.regex.*;
-
-class EvaluateExpression {
-    String expr = "";
-
-    EvaluateExpression(String expression) {
-        this.expr = expression;
-    }
-
-    void sayHello() {
-        System.out.println(this.expr);
-    }
-
-}
 
 class ExpressionEvaluator {
 
+    static ArrayList<String> tokenizeExpression(String expr) {
+        String pattern = "(?<=[-+*/])|(?=[-+*/])";
+        String[] array = expr.split(pattern);
+        return new ArrayList<>(Arrays.asList(array));
+    }
+
+    static String eval(String a, String b, String op) {
+        double result = 0;
+
+        double A = Double.parseDouble(a);
+        double B = Double.parseDouble(b);
+
+        switch (op) {
+        case "*":
+            result = A * B;
+            break;
+        case "/":
+            if (B != 0) {
+                result = A / B;
+            }
+            break;
+        case "+":
+            result = A + B;
+            break;
+        case "-":
+            result = A - B;
+            break;
+        }
+        return String.valueOf(result);
+    }
+
+    static ArrayList<String> evalHighPrecedence(ArrayList<String> expr) {
+        int i = 0;
+        while (i < expr.size()) {
+            int l = i - 1;
+            int r = i + 1;
+
+            if (expr.get(i).equals("*") || expr.get(i).equals("/")) {
+                // Set eval's result in expr arraylist
+                expr.set(i, eval(expr.get(l), expr.get(r), expr.get(i)));
+
+                // remove nums after using
+                expr.remove(r);
+                expr.remove(l);
+            }
+            i++;
+        }
+
+        System.out.println(expr);
+        return expr;
+    }
+
+    static ArrayList<String> evalLowPrecedence(ArrayList<String> expr) {
+        int i = 1;
+        while (expr.size() > 1) {
+            int l = i - 1;
+            int r = i + 1;
+
+            // Set eval's result in expr arraylist
+            expr.set(i, eval(expr.get(l), expr.get(r), expr.get(i)));
+
+            // remove nums after using
+            expr.remove(r);
+            expr.remove(l);
+        }
+
+        System.out.println(expr);
+
+        return expr;
+    }
+
     public static void main(String[] args) {
         String expr = "5+4*12-100+52/2";
-        String[] nums = expr.split("(?<=[-+*/])|(?=[-+*/])");
-        for (String c : nums) {
-            System.out.println(c);
-        }
+
+        ArrayList<String> tokenizedExpr = tokenizeExpression(expr);
+
+        ArrayList<String> postHigh = evalHighPrecedence(tokenizedExpr);
+        ArrayList<String> postLow = evalLowPrecedence(postHigh);
+
     }
 }
